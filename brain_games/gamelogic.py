bcb2fa4d7aad719271cee182152da7cf7c89fc09
kutukas <freschers@gmail.com>
2021-01-16 14:23:12 +0100

    correct the lint mistakes

--- brain_games/gamelogic.py
+++ brain_games/gamelogic.py
@@ -3,7 +3,7 @@ import random
 
 
 def is_even(number):
-    #return 'yes' if a number is even and 'no' if odd
+    # return 'yes' if a number is even and 'no' if odd
     if (number % 2) == 0:
         return 'yes'
     return 'no'
@@ -13,7 +13,7 @@ def game_is_even():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
-    cac = 0 # cac - correct answer counter
+    cac = 0  # cac - correct answer counter
     while cac < 3:
         random_number = random.randrange(99)
         print('Question: {}'.format(random_number))
@@ -23,9 +23,9 @@ def game_is_even():
             print('Correct!')
             cac += 1
         else:
-            print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(answer, correct_answer))
+            print(("'{}' is wrong answer ;(. "
+                   "Correct answer was '{}'.").format(answer, correct_answer))
             print("Let's try again, {}!".format(name))
             break
     else:
         print('Congratulations, {}!'.format(name))
-
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,5 +5,6 @@ from .. import gamelogic
 def main():
     gamelogic.game_is_even()
 
+
 if __name__ == '__main__':
     main()
